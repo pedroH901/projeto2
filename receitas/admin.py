79d3ca777092682fbfca4e189475861a3d42e11b
Pedro <phdf2016@gmail.com>
2025-08-26 18:37:23 -0300

    push

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Receita
 
+admin.site.register(Receita)
 # Register your models here.
--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Receita
 
 # Create your views here.
 def home(request):
     return render(request, 'receitas/home.html')
+
+def receita_detail(request, id):
+    receita = get_object_or_404(Receita, pk=id)
+    context = {
+        'receita': receita
+    }
+
+    return render(request, 'receitas/receita_detail.html', context)
